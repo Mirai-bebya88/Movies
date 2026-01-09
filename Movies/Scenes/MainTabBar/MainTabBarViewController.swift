185f4eb0bf8537cdcddb52f3b527032744d6badd
Mirai-bebya88 <malakmadze.elene@gmail.com>
2026-01-09 15:51:01 +0400

    Initial commit

---
+++ Movies/Scenes/MainTabBar/MainTabBarViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MainTabBarViewController.swift
+//  Movies
+//
+//  Created by elene malakmadze on 09.01.26.
+//
+
+import UIKit
+
+class MainTabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+}
